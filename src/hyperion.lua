d38ba78bd875c519a6237b3da216c324cab7b003
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-09 01:00:43 +0800

    sync

--- src/hyperion.lua
+++ src/hyperion.lua
@@ -31,7 +31,7 @@ do
   else
     local pending = 0
     for _, item in ipairs(result) do
-      print(table.dump(item))
+      print(item)
       if item.type == "file" then
         pending += 1
         task.spawn(function()
