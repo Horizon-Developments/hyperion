fcfe5f2b2232c003483584e9078d2b7187cbd237
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-09 00:37:21 +0800

    sync

--- src/hyperion.lua
+++ src/hyperion.lua
@@ -31,7 +31,7 @@ do
   else
     for i, item in ipairs(result) do
       if item.type == "file" then
-        task.spawn(function()
+        function()
           local ok, err = pcall(function()
             writefile(assets("modules", item.name), game:HttpGet(item.download_url))
           end)
