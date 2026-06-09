49a960eb78ece8be802855db2d0fce358ae39b56
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-10 00:01:50 +0800

    sync

--- assets/modules/spychat.lua
+++ assets/modules/spychat.lua
@@ -21,36 +21,6 @@ local tcs = game:GetService("TextChatService")
 local localplr = plrs.LocalPlayer
 local tab = tabs.adminkit
 local toggles = {}
-local function cmd(c)
-  tcs.TextChannels.RBXGeneral:SendAsync(";"..c.." [HYPERION REBORN]")
-end
-
-toggles.antijoin = {}
-
-tab:Dropdown({
-  Title = "Anti join*",
-  Desc = "Prevents join og, vc, xl (:",
-  Values = { "joinxl", "joinog", "joinvc" },
-  Value = {},
-  Multi = true,
-  Callback = function(selected)
-    toggles.antijoin = selected
-  end
-})
-
-tcs.MessageReceived:Connect(function(msg)
-  if not msg.TextSource or msg.TextSource.UserId == localplr.UserId then return end
-  local text = msg.Text:lower():gsub("%s+", "")
-  for _, v in ipairs(toggles.antijoin) do
-    local keyword = v:lower()
-    if text == keyword or text:sub(1, #keyword) == keyword or text:find(keyword, 1, true) then
-      cmd("reset " .. msg.TextSource.Name)
-      break
-    end
-  end
-end)
-
-
 
 local tcmp = Instance.new("TextChatMessageProperties")
   tcs.OnIncomingMessage = function(m)
