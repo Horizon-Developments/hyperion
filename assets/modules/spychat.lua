f6e59f97c21e56806f66133a8393d6051f7312e8
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-10 00:03:12 +0800

    sync

--- assets/modules/spychat.lua
+++ assets/modules/spychat.lua
@@ -23,11 +23,7 @@ local tab = tabs.spychat
 local toggles = {}
 
 local tcmp = Instance.new("TextChatMessageProperties")
-tcs.OnIncomingMessage = function(m)
-  if m.Text and m.Text:sub(1, 3) == "hx." and m.TextSource and m.TextSource.UserId == LocalPlayer.UserId then
-    task.spawn(handler, m)
-    m.Text = ""
-  end
+tcs.OnIncomingMessage = function(m) 
   return tcmp
 end
 
