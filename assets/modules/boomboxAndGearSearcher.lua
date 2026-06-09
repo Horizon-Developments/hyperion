eb9840078e537427fb3e0dcf04aa137f3b7b66da
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-09 22:19:03 +0800

    sync

--- assets/modules/boomboxAndGearSearcher.lua
+++ assets/modules/boomboxAndGearSearcher.lua
@@ -64,6 +64,7 @@ tab:Input({
       Method = "GET"
     })
   if not ok or not res or res.StatusCode ~= 200 or not res.Body then
+    print("[HYPERION] ", not ok, not res, res.StatusCode ~= 200 or not res.Body)
     WindUI:Notify({ Title = "Not found", Content = "An error happend and was logged.", Duration = 3 })
   end
   
