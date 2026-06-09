653a322bf17df74258be3c7822a10f481d7c4af1
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-09 21:53:05 +0800

    sync

--- assets/modules/boomboxAndGearSearcher.lua
+++ assets/modules/boomboxAndGearSearcher.lua
@@ -50,3 +50,35 @@ tab:Input({
     })
   end
 })
+tab:Input({
+  Title = "Gear searcher",
+  Desc = "Search gears here!",
+  Value = "",
+  InputIcon = "music",
+  Type = "Input",
+  Placeholder = "Enter keyword here",
+  Callback = function(keyword)
+    if (keyword == "") then return end
+    local params = Instance.new("AudioSearchParams")
+    params.SearchKeyword = keyword
+    local results = AssetService:SearchAudioAsync(params):GetCurrentPage() 
+    if (#results <= 0) then
+      WindUI:Notify({ Title = "Not found", Content = "Nothing came up! try using another keyword.", Duration = 3 })
+      return
+    end
+    if (cache.dropdownBB) then cache.dropdownBB:Destroy() end
+    cache.dropdownBB = tab:Dropdown({
+      Title = "Copy here",
+      Desc = "",
+      Values = results,
+      Value = nil,
+      AllowNone = true,
+      Callback = function(option) 
+        if (option == nil) then return end
+        setclipboard(tostring(option.Id))
+        WindUI:Notify({ Title = "Copied!", Content = "Copied to your clipboard.", Duration = 3 })
+        Dropdown:Select(nil)
+      end
+    })
+  end
+})
