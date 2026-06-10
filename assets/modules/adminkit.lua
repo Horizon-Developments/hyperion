694c2741f5b8d81ea4c1bc1bddf7487777bcb252
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-10 23:55:12 +0800

    sync

--- assets/modules/adminkit.lua
+++ assets/modules/adminkit.lua
@@ -1,31 +1,21 @@
 local args = ...
 
 local tabs = args.Tabs
--- tabs register or use tabs here.
 local Window = args.Window
--- wind ui window used by Hyperion
 local WindUI = args.WindUI
--- WindUi 
 local assets = args.Assets
--- store files in assets("your folder") (don't forget to run makefolder though)
-
-
+local Helpers = args.Helpers
 
 tabs.adminkit = Window:Tab({
   Title = "AdminKit",
   Icon = "wrench"
 })
 
-local plrs = game:GetService("Players")
-local tcs = game:GetService("TextChatService")
-local localplr = plrs.LocalPlayer
 local tab = tabs.adminkit
-local toggles = {}
-local function cmd(c)
-  tcs.TextChannels.RBXGeneral:SendAsync(";"..c.." [HYPERION REBORN]")
-end
+local tcs = Helpers.services.textchat
+local localplr = Helpers.services.players.LocalPlayer
+local toggles = { antijoin = {} }
 
-toggles.antijoin = {}
 
 tab:Dropdown({
   Title = "Anti join*",
@@ -38,13 +28,13 @@ tab:Dropdown({
   end
 })
 
-tcs.MessageReceived:Connect(function(msg)
+Helpers.on("ChatListener", function(msg)
   if not msg.TextSource or msg.TextSource.UserId == localplr.UserId then return end
   local text = msg.Text:lower():gsub("%s+", "")
   for _, v in ipairs(toggles.antijoin) do
     local keyword = v:lower()
     if text == keyword or text:sub(1, #keyword) == keyword or text:find(keyword, 1, true) then
-      cmd("reset " .. msg.TextSource.Name)
+      Helpers.cmd("reset " .. msg.TextSource.Name)
       break
     end
   end
