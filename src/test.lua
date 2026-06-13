8e2423a5dca46e42adb68325450ba88ee8417631
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-13 18:25:03 +0800

    sync

--- src/test.lua
+++ src/test.lua
@@ -1,18 +1,24 @@
-local builder = loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon-Developments/hyperion/refs/heads/main/src/autobuild.lua"))()
+local autobuild = loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon-Developments/hyperion/refs/heads/main/src/autobuild.lua"))()
+
 if isfile("HYPBuild.lz4") then
-  print("LOAD BUILD")
-  local builder = autobuild.build("mybuild", {
-    tp = true,
-    offset = Vector3.new(0,0,0)
-  }, function(tool)
-    return game.Players.LocalPlayer.Backpack:FindFirstChild(tool, true) or game.Players.LocalPlayer.Character:FindFirstChild(tool, true)
-  end)
-  builder:start()
-  print("DONE")
+    print("LOAD BUILD")
+
+    local builder = autobuild.build("mybuild", {
+        tp = true,
+        offset = Vector3.new(0, 0, 0)
+    }, function(tool)
+        return game.Players.LocalPlayer.Backpack:FindFirstChild(tool, true)
+            or game.Players.LocalPlayer.Character:FindFirstChild(tool, true)
+    end)
+
+    builder:start()
+    print("DONE")
 else
-  print("SAVE BUILD")
-  autobuild.save("mybuild", {
-    game.Players.LocalPlayer
-  })
-  print("DONE")
+    print("SAVE BUILD")
+
+    autobuild.save("mybuild", {
+        game.Players.LocalPlayer
+    })
+
+    print("DONE")
 end
