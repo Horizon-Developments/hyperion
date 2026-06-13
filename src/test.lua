2458bdf62b0bedae57fa6e6786879238274f7e51
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-13 18:22:39 +0800

    sync

--- src/test.lua
+++ src/test.lua
@@ -1,9 +1,13 @@
 local builder = loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon-Developments/hyperion/refs/heads/main/src/autobuild.lua"))()
 if isfile("HYPBuild.lz4") then
   print("LOAD BUILD")
-  autobuild.save("mybuild", {
-    game.Players.LocalPlayer
-})
+  local builder = autobuild.build("mybuild", {
+    tp = true,
+    offset = Vector3.new(0,0,0)
+}, function(tool)
+    return game.Players.LocalPlayer.Backpack:FindFirstChild(tool, true)
+        or game.Players.LocalPlayer.Character:FindFirstChild(tool, true)
+end)
   print("DONE")
 else
   print("SAVE BUILD")
