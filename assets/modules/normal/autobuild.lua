cb943b6fc961eb9a3636bc24709d4267ea896495
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-13 23:38:06 +0800

    sync

--- assets/modules/normal/autobuild.lua
+++ assets/modules/normal/autobuild.lua
@@ -161,7 +161,9 @@ tab:Button({
     end
     local instance;
     task.spawn(function()
-      instance = lib.build(SAVE_DIR .. "/" .. file, )
+      instance = lib.build(SAVE_DIR .. "/" .. file, {
+        
+      })
     end)
     
   end
