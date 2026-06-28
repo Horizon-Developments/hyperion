7cc785300dd7ce8d478cb8b6cbcac4ace74206f8
Horizon-Developments <thehyperiondev@gmail.com>
2026-06-28 20:09:29 +0800

    update

--- src/loader.lua
+++ src/loader.lua
@@ -1,6 +1,10 @@
+--[[
 local fn, err = loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon-Developments/hyperion/refs/heads/main/src/hyperion.lua"))
 print("[LOADER]: " ,fn, err, fn())
 if not ok then
   print("[LOADER]: Report this error in our discord server: discord.gg/xbkVzSxDBy\n screenshot the error")
   game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
 end
+]]
+print("[LOADER]: Hyperion is DOWN. our discord server: discord.gg/xbkVzSxDBy\nwait.")
+  game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
